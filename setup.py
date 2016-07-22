a7b7bbfa8947d1c5e8913005cd5f332125eb813a
dongkai <dongkai@beyondcent.com>
2016-07-22 13:41:56 +0800

    plugin

---
+++ setup.py
@@ -0,0 +1,16 @@
+__author__ = 'dongkai'
+from setuptools import setup
+setup(
+  zip_safe=True,
+  name='cloudify-mogi-plugin',
+  version='1.0',
+  author='dongkai',
+  author_email='dongkai@beyondcent.com',
+  packages=[
+    'lib'
+  ],
+  license='LICENSE',
+  description='Playground plugins',
+  install_requires=[
+  ]
+)
---
+++ sources/plugin.py
@@ -0,0 +1,7 @@
+from cloudify import ctx
+from cloudify.decorators import operation
+
+
+@operation
+def create(**kwargs):
+    ctx.logger.info('ECHO PLUGIN: create running')
